0045f3e5b9e7f38027cf2bb5339a83406766e49f
ITPC <pcking60@gmail.com>
2023-04-26 08:45:08 +0700

    add_post_req

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+  res.send("Hello World! - posted!");
+});
+
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
